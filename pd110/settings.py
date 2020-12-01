acbaa41b172314b7b7407f7098128560c9a87adf
kmvp <kari_oc3@hotmail.com>
2020-12-01 02:14:03 -0600

    Update settings.py

--- pd110/settings.py
+++ pd110/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = []
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'kari.oc3@gmail.com'
-EMAIL_HOST_PASSWORD = 'Florecita29..'
+EMAIL_HOST_PASSWORD = '**********'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
